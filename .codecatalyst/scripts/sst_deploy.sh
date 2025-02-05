41d9cabdb787e96ffc02f0e67c362448a7fa55ec
Mohamed Alkawari <mk.alkawari@gmail.com>
2025-02-05 19:14:57 +0300

    Update sst_deploy.sh

--- .codecatalyst/scripts/sst_deploy.sh
+++ .codecatalyst/scripts/sst_deploy.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 echo "Deploying project"
 
-source ~/.bashrc
-nohup dockerd &
-docker version
 npm install
 npm install react-dropzone
 npm install @heroicons/react
 npm install chart.js
-npx sst deploy --stage prod --force --cloudformation-execution-policies arn:aws:iam::600627328431:policy/Cdk_Least --template iGA_CloudFormation_Tamplate.yaml
+
+npx cdk bootstrap aws://600627328431/us-east-1 --force \
+  --cloudformation-execution-policies arn:aws:iam::600627328431:policy/Cdk_Least \
+  --no-public-access-block-configuration
+
+npx sst deploy --stage prod
