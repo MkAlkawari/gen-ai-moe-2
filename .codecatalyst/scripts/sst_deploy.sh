0163405efdd9c67977cafaae8a9a72c5cb439e98
Mohamed Alkawari <mk.alkawari@gmail.com>
2025-02-07 00:33:19 +0300

    Update sst_deploy.sh

--- .codecatalyst/scripts/sst_deploy.sh
+++ .codecatalyst/scripts/sst_deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 echo "Deploying project"
 
+source ~/.bashrc
+nohup dockerd &
+docker version
 npm install
-npm install react-dropzone
-npm install @heroicons/react
-npm install chart.js
+npm audit fix
 
 npx cdk bootstrap aws://600627328431/us-east-1 --force \
   --cloudformation-execution-policies arn:aws:iam::600627328431:policy/Cdk_Least \
